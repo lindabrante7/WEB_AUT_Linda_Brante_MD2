b8c5bddb448e415ae6d5ecff9323744abefa33aa
Linda <lindabrante47@gmail.com>
2025-05-27 16:57:13 +0300

    add code for make_appointment file

---
+++ cypress/e2e/make_appointment.cy.js
@@ -0,0 +1,25 @@
+    import loginPage from '../../Pages/cura/loginPage';
+    import appointmentPage from '../../pages/cura/appointmentPage';
+
+    describe('Make an Appointment', () => {
+    it('should make an appointment successfully', () => {
+        loginPage.visit();
+        loginPage.clickMakeAppointment();
+        loginPage.login('John Doe', 'ThisIsNotAPassword');
+
+        appointmentPage.selectFacility('Seoul CURA Healthcare Center');
+        appointmentPage.checkReadmission();
+        appointmentPage.selectHealthcareProgram('Medicaid');
+        appointmentPage.selectDate('30');
+        appointmentPage.setComment('CURA Healthcare Service');
+        appointmentPage.bookAppointment();
+
+        appointmentPage.validateSummary({
+        facility: 'Seoul CURA Healthcare Center',
+        readmission: 'Yes',
+        program: 'Medicaid',
+        date: '30/05/2025',
+        comment: 'CURA Healthcare Service'
+        });
+    });
+    });
